c1d935a3c8219ed3d6786ff6deb3c1686ed99fe1
Ogkrn <krnjoshi1542@gmail.com>
2025-09-14 21:31:15 +0530

    /dist/index.js updated

--- college-qr-system/apps/backend/dist/index.js
+++ college-qr-system/apps/backend/dist/index.js
@@ -2,6 +2,12 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env["PORT"] || 4000;
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("🚀 Server is working!");
+});
+
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
